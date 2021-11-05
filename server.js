2fdb4c2ab5eff384b65d89ff75920549c6d74af9
@biancac <>
2021-11-05 20:45:12 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+//console.log('Hello!')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const routes = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+
+server.use(middlewares);
+server.use(routes);
+server.listen(port);
